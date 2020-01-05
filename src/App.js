60ebc436b65aaea13ef3446b3028096ad0ed6f12
knhbae <knhbae@gmail.com>
2020-01-05 20:52:18 +0900

    2번째 커밋

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+// import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "http://placeimg.com/64/64/1",
+    name: "skehdqls",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 2,
+    image: "http://placeimg.com/64/64/2",
+    name: "skehdqls",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생"
+  },
+  {
+    id: 3,
+    image: "http://placeimg.com/64/64/3",
+    name: "skehdqls",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생"
+  }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
